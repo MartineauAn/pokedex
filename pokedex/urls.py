6161a322c5ab40b623c2a6f76d752ccf9a1ce28c
martineauAn <martineau.antoine2001@gmail.com>
2022-10-31 18:48:44 +0100

    rectifs

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("pokedexapp/",include("pokedexapp.urls"))
+    path("",include("pokedexapp.urls"))
 ]
