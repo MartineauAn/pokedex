1725623e9795e8f347a14e14e57b7faf37993e66
martineauAn <martineau.antoine2001@gmail.com>
2022-11-17 14:22:08 +0100

    front dashboard + camera

--- pokedexapp/static/pokedexapp/bg.js
+++ pokedexapp/static/pokedexapp/bg.js
@@ -12,9 +12,10 @@ camera.position.setZ(30);
 
 var loader = new GLTFLoader();
 loader.load(
-   chemin + "/objects/6.glb",
+   chemin + "/objects/133.glb",
    function ( gltf ) {
-        gltf.scene.rotateY(0.60);
+        gltf.scene.scale.set(0.1,0.1,0.1);
+        fitCameraToCenteredObject(camera,gltf.scene,100,controls)
         scene.add(gltf.scene);
    },
 );
@@ -52,6 +53,110 @@ function animate(){
 
     renderer.render(scene, camera);
 }
+
+const size = new THREE.Vector3();
+const center = new THREE.Vector3();
+const box = new THREE.Box3();
+
+function fitCameraToSelection(camera, controls, selection, fitOffset = 1.2) {
+    box.makeEmpty();
+    box.expandByObject(selection);
+    
+    box.getSize(size);
+    box.getCenter(center );
+    
+    const maxSize = Math.max(size.x, size.y, size.z);
+    const fitHeightDistance = maxSize / (2 * Math.atan(Math.PI * camera.fov / 360));
+    const fitWidthDistance = fitHeightDistance / camera.aspect;
+    const distance = fitOffset * Math.max(fitHeightDistance, fitWidthDistance);
+    
+    const direction = controls.target.clone()
+        .sub(camera.position)
+        .normalize()
+        .multiplyScalar(distance);
+
+    controls.maxDistance = distance * 10;
+    controls.target.copy(center);
+    
+    camera.near = distance / 100;
+    camera.far = distance * 100;
+    camera.updateProjectionMatrix();
+
+    camera.position.copy(controls.target).sub(direction);
+    
+    controls.update();
+}
+
+const fitCameraToCenteredObject = function (camera, object, offset, orbitControls ) {
+    const boundingBox = new THREE.Box3();
+    boundingBox.setFromObject( object );
+
+    var middle = new THREE.Vector3();
+    var size = new THREE.Vector3();
+    boundingBox.getSize(size);
+
+    // figure out how to fit the box in the view:
+    // 1. figure out horizontal FOV (on non-1.0 aspects)
+    // 2. figure out distance from the object in X and Y planes
+    // 3. select the max distance (to fit both sides in)
+    //
+    // The reason is as follows:
+    //
+    // Imagine a bounding box (BB) is centered at (0,0,0).
+    // Camera has vertical FOV (camera.fov) and horizontal FOV
+    // (camera.fov scaled by aspect, see fovh below)
+    //
+    // Therefore if you want to put the entire object into the field of view,
+    // you have to compute the distance as: z/2 (half of Z size of the BB
+    // protruding towards us) plus for both X and Y size of BB you have to
+    // figure out the distance created by the appropriate FOV.
+    //
+    // The FOV is always a triangle:
+    //
+    //  (size/2)
+    // +--------+
+    // |       /
+    // |      /
+    // |     /
+    // | F° /
+    // |   /
+    // |  /
+    // | /
+    // |/
+    //
+    // F° is half of respective FOV, so to compute the distance (the length
+    // of the straight line) one has to: `size/2 / Math.tan(F)`.
+    //
+    // FTR, from https://threejs.org/docs/#api/en/cameras/PerspectiveCamera
+    // the camera.fov is the vertical FOV.
+
+    const fov = camera.fov * ( Math.PI / 180 );
+    const fovh = 2*Math.atan(Math.tan(fov/2) * camera.aspect);
+    let dx = size.z / 2 + Math.abs( size.x / 2 / Math.tan( fovh / 2 ) );
+    let dy = size.z / 2 + Math.abs( size.y / 2 / Math.tan( fov / 2 ) );
+    let cameraZ = Math.max(dx, dy);
+
+    // offset the camera, if desired (to avoid filling the whole canvas)
+    if( offset !== undefined && offset !== 0 ) cameraZ *= offset;
+
+    camera.position.set( 0, 0, cameraZ );
+
+    // set the far plane of the camera so that it easily encompasses the whole object
+    const minZ = boundingBox.min.z;
+    const cameraToFarEdge = ( minZ < 0 ) ? -minZ + cameraZ : cameraZ - minZ;
+
+    camera.far = cameraToFarEdge * 3;
+    camera.updateProjectionMatrix();
+
+    if ( orbitControls !== undefined ) {
+        // set camera to rotate around the center
+        orbitControls.target = new THREE.Vector3(0, 0, 0);
+
+        // prevent camera from zooming out far enough to create far plane cutoff
+        orbitControls.maxDistance = cameraToFarEdge * 2;
+    }
+};
+
 
 animate();
 
